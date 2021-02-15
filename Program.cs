4c21325eb8f045bcc5ca204ca2ce661d3b834471
Daniel Antunes <danielxts555@gmail.com>
2021-02-15 19:38:10 -0300

    git init

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Pessoa fulano_1, fulano_2;
+
+            fulano_1 = new Pessoa();
+            fulano_2 = new Pessoa();
+
+         Console.WriteLine("Dados da primeira pessoa:");
+         Console.Write("Nome:");
+         fulano_1.Nome = Console.ReadLine();
+         Console.Write("Idade:");
+         fulano_1.Idade = int.Parse(Console.ReadLine());
+         
+
+         Console.WriteLine("Dados da segunda pessoa:");
+         Console.Write("Nome:");
+         fulano_2.Nome = Console.ReadLine();
+         Console.Write("Idade:");
+         fulano_2.Idade = int.Parse(Console.ReadLine());
+
+            if ( fulano_1.Idade > fulano_2.Idade)
+            {
+                Console.WriteLine("Pessoa mais velha:"+ fulano_1.Nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha:" + fulano_2.Nome);
+            }
+        }
+        
+
+      
+        
+    }
+}
